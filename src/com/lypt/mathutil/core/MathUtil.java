2a1330d2a00dc4bc20e56cc92971fcbfcf16e576
lypt16 <lyphung1611@gmail.com>
2024-07-16 10:00:12 +0700

    Refactor the main code and green 4ever always

--- src/com/lypt/mathutil/core/MathUtil.java
+++ src/com/lypt/mathutil/core/MathUtil.java
@@ -18,16 +18,36 @@ public class MathUtil {
     //không có giai thừa âm
     //0! = 1! = 1 quy ước
     // tính n! trong khoảng từ 0...20
-    public static long getFactorial(int n){
-        if(n < 0 || n > 20)
-            throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
+//    public static long getFactorial(int n){
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
+//        if (n == 0 || n == 1)
+//            return 1; // có return trong if rồi nên sẽ không dùng else trong trường hợp còn lại
+//        
+//        long product = 1; //product: tích, tích nhân dồn
+//        for (int i = 2; i <= n; i++) 
+//            product *= i; 
+//        return product;           
+//    }
+    
+    //Đệ quy (Recursion): gọi lại chính mình với 1 quy mô khác
+    //n! = 1.2.3.4.5.6. ... . n
+    // 6! = 6 x 5!
+    // 5! = 5 x 4!
+    // 4! = 4 x 3!
+    // 3! = 3 x 2!
+    // 2! = 2 x 1! // điểm dừng
+    // 1! = 1
+    
+    //quy ước 1! = 0! - 1;
+    //=> n! = n * (n - 1)!
+    
+        public static long getFactorial(int n){
+        
         if (n == 0 || n == 1)
             return 1; // có return trong if rồi nên sẽ không dùng else trong trường hợp còn lại
-        
-        long product = 1; //product: tích, tích nhân dồn
-        for (int i = 2; i <= n; i++) 
-            product *= i; 
-        return product;           
+       
+        return n * getFactorial(n-1); //công thức đệ quy 
     }
-    
+
 }
